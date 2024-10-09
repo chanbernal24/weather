fc3d28191a27dcc51086dad1cc580d30ac9ebe1f
Christian Bernal <bernal.johnchristian.08242003@gmail.com>
2024-10-09 20:09:56 +0800

    ui done

--- src/index.js
+++ src/index.js
@@ -3,4 +3,33 @@ import * as bootstrap from 'bootstrap'
 import './styles.css'
 
 
-console.log("Hello World!");
+let getData = async () => {
+    try {
+        let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/manila?unitGroup=us&key=CYGZ6QWJKWSPTT82C7JP2D9Z2&contentType=json"
+        let get = await fetch(url)
+        let data = await get.json()
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+
+getData()
+
+// let date = new Date("2024-10-09")
+// let day = date.getDay();
+// let whatDay;
+// switch (day) {
+//     case 3: 
+//         whatDay = "Wednesday"
+//         break
+//     default:
+//         console.log("err")
+//         break
+// }
+
+
+
+// getData()
+// console.log(whatDay)
