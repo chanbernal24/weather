d34c9b45d96e5727f9025036183d22aa97d801ed
Christian Bernal <bernal.johnchristian.08242003@gmail.com>
2024-10-10 15:27:53 +0800

    location controller initiated in index.js for testing

--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
 import './/scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 import './styles.css'
-
-
-let getData = async () => {
-    try {
-        let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/manila?unitGroup=us&key=CYGZ6QWJKWSPTT82C7JP2D9Z2&contentType=json"
-        let get = await fetch(url)
-        let data = await get.json()
-        console.log(data)
-    } catch (err) {
-        console.log(err)
-    }
-
-}
-
-getData()
-
-// let date = new Date("2024-10-09")
-// let day = date.getDay();
-// let whatDay;
-// switch (day) {
-//     case 3: 
-//         whatDay = "Wednesday"
-//         break
-//     default:
-//         console.log("err")
-//         break
-// }
+import LocationData from './models/location_data_model'
+import LocationController from './controllers/location_controller'
 
 
 
-// getData()
-// console.log(whatDay)
+let locationController = new LocationController()
