7f6982038a609562a5b22b5d0dd1a5d27e07a74c
Christian Bernal <bernal.johnchristian.08242003@gmail.com>
2024-10-14 19:18:41 +0800

    added weather card component

---
+++ src/views/components/weather_card_component.js
@@ -0,0 +1,43 @@
+class WeatherCardComponent {
+    constructor(day, imageSrc, temp) {
+        this.day = day,
+        this.imageSrc = imageSrc,
+        this.temp = temp
+
+    }
+
+
+    cardComponent = () => {
+        let cardContainer = document.createElement('div')
+        cardContainer.setAttribute('class', 'card p-5 border border-0')
+        cardContainer.setAttribute('style', 'width: 8rem;')
+
+        let cardElementsContainer = document.createElement('div')
+        cardElementsContainer.setAttribute('class', 'd-flex align-items-center justify-content-center flex-column')
+
+        let day = document.createElement('h6')
+        day.setAttribute('class', 'mb-3')
+        day.textContent = this.day
+
+        let imageWeatherIcon = document.createElement('img')
+        imageWeatherIcon.setAttribute('src', this.imageSrc)
+        imageWeatherIcon.setAttribute('class', 'mb-3')
+        imageWeatherIcon.setAttribute('width', '52')
+        imageWeatherIcon.setAttribute('height', '52')
+
+        let temperature = document.createElement('small')
+        temperature.setAttribute('class', 'fw-light')
+        temperature.textContent = `${this.temp}°C`
+
+        cardElementsContainer.append(day, imageWeatherIcon, temperature)
+        cardContainer.appendChild(cardElementsContainer)
+
+        return cardContainer
+
+        
+    }
+
+
+}
+
+export default WeatherCardComponent;
