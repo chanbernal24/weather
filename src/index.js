9e7c7fe5ec1062cc5580e80301145b17f0b3ab79
Christian Bernal <bernal.johnchristian.08242003@gmail.com>
2024-10-14 21:23:08 +0800

    dynamic ui commented out temporarily to not fetch data after modifying files

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ import LocationController from './controllers/location_controller'
 
 
 // let locationController = new LocationController()
+let data = new Date("2024-10-14")
+console.log(data.getDay())
